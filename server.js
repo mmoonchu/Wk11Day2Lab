c6bfb815e5f4b3abe89622cfe014a22d6f346d0e
Moon Chu <mmoonchu2@gmail.com>
2023-12-14 10:33:01 -0500

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const app = express();
 app.listen('3000', (req, res) => {
     console.log("Server is currently running on port 3000");
 })
+
 app.get( '/', (req, res) => {
     res.send('<h1>Yello World</h1>');
 } );
 app.get( '/greeting/:name', (req, res) => {
-    res.send(`<h1>Hello, ${req.params.name}</h1>`)
+    res.send(`<h1>Hello, ${req.params.name}</h1>`);
+})
+
+app.get( '/tip/:total/:tipPercentage', (req, res) => {
+    function calcTip(total, percentage) {
+        return (total*(percentage/100));
+    }
+    res.send(`<h1>${calcTip(req.params.total, req.params.tipPercentage)}</h1>`)
 })
